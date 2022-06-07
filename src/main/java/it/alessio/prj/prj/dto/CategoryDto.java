c3745ac5fb5a25fe16199a1a001ad2a3212ba975
PC0GF34A-TO\alessio.garbi <alessio.garbi@agmsolutions.net>
2022-06-07 16:30:47 +0200

    populate category

--- src/main/java/it/alessio/prj/prj/dto/CategoryDto.java
+++ src/main/java/it/alessio/prj/prj/dto/CategoryDto.java
@@ -1,3 +1,28 @@
 package it.alessio.prj.prj.dto;
 
-public class CategoryDto {}
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto implements Serializable {
+
+  private Integer id;
+
+  private String name;
+
+  private boolean attivo;
+
+  private String description;
+
+  private Date dataCreazione;
+
+  private Date dataModifica;
+}
---
+++ src/main/resources/db/migration/V3__insert_category.sql
@@ -0,0 +1,6 @@
+INSERT INTO  `category`
+    (`name`,`description`,`attivo`)
+    VALUES
+    ("Turismo", "Categoria ambito turistico", 1),
+    ("Bancario", "Categoria ambito bancario", 1),
+    ("Assicurativo", "Categoria ambito assicurativo", 1);
